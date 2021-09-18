99838b52840ad1edb73d124dd523581743ea08b7
Linar Yusupov <linar.r.yusupov@gmail.com>
2021-09-18 19:49:04 +0300

    POST sketch is updated to work with T-Impulse product as well

--- POST_S76G.ino
+++ POST_S76G.ino
@@ -33,6 +33,7 @@
 #define BMP085_I2CADDR        0x77
 #define BMP280_ADDRESS        0x77
 #define BMP280_ADDRESS_ALT    0x76 /* GY-91, SA0 is NC */
+#define ICM20948_ADDRESS      0x69
 
 #define BMP280_REGISTER_CHIPID 0xD0
 
@@ -76,6 +77,17 @@
 #define SOC_GPIO_PIN_SDA      PB7
 #define SOC_GPIO_PIN_SCL      PB6
 
+#define TTGO_TIMPULSE_OLED_PIN_RST PA8
+#define TTGO_TIMPULSE_GPIO_PAD_OUT PA0
+#define TTGO_TIMPULSE_GPIO_PAD_PWR PA2
+#define TTGO_TIMPULSE_SENSOR_INT   PB1
+#define TTGO_TIMPULSE_GPIO_CHRG    PB8
+#define TTGO_TIMPULSE_VDD_1V8_EN   PB0
+#define TTGO_TIMPULSE_GPS_PWR_EN   PA3
+
+/* button */
+#define SOC_GPIO_PIN_BUTTON   TTGO_TIMPULSE_GPIO_PAD_OUT
+
 #define GNSS_BAUD_RATE        115200
 
 HardwareSerial Serial1(SOC_GPIO_PIN_CONS_RX, SOC_GPIO_PIN_CONS_TX);
@@ -84,6 +96,10 @@ HardwareSerial Serial3(SOC_GPIO_PIN_GNSS_RX, SOC_GPIO_PIN_GNSS_TX);
 U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8_i2c(U8X8_PIN_NONE);
 
 static U8X8_SSD1306_128X64_NONAME_HW_I2C *u8x8 = NULL;
+
+const char SoftRF_text1[]  = "SoftRF";
+const char SoftRF_text2[]  = "and";
+const char SoftRF_text3[]  = "LilyGO";
 
 static boolean GNSS_probe() {
 
@@ -156,12 +172,21 @@ static bool bmp_probe()
   return false;
 }
 
+static bool imu_probe()
+{
+  Wire.beginTransmission(ICM20948_ADDRESS);
+  if (Wire.endTransmission() == 0) return true;
+
+  return false;
+}
+
 void setup() {
 
   bool has_SX1276 = false;
   bool has_GNSS   = false;
   bool has_OLED   = false;
   bool has_BMP280 = false;
+  bool has_IMU    = false;
 
   Serial.begin(115200);
 
@@ -172,14 +197,36 @@ void setup() {
   delay(500);
 #endif
 
-  Serial.println();
-  Serial.println(F("TTGO T-Motion (S76G) Power-on Self Test"));
-  Serial.println();
-  Serial.flush();
+  pinMode(TTGO_TIMPULSE_VDD_1V8_EN, INPUT_PULLUP);
+  delay(150);
+  pinMode(TTGO_TIMPULSE_GPS_PWR_EN, INPUT_PULLUP);
+  delay(50);
 
   Wire.setSCL(SOC_GPIO_PIN_SCL);
   Wire.setSDA(SOC_GPIO_PIN_SDA);
   Wire.begin();
+
+  has_IMU = imu_probe();
+
+  if (has_IMU) {
+    pinMode(TTGO_TIMPULSE_GPIO_PAD_PWR, INPUT_PULLUP);
+
+    pinMode(TTGO_TIMPULSE_OLED_PIN_RST, INPUT_PULLDOWN);
+    delay(200);
+    pinMode(TTGO_TIMPULSE_OLED_PIN_RST, INPUT_PULLUP);
+
+    pinMode(TTGO_TIMPULSE_SENSOR_INT, INPUT);
+  } else {
+    pinMode(TTGO_TIMPULSE_VDD_1V8_EN, INPUT_ANALOG);
+    pinMode(TTGO_TIMPULSE_GPS_PWR_EN, INPUT_ANALOG);
+  }
+
+  Serial.println();
+  Serial.print(F("TTGO "));
+  Serial.print(has_IMU ? F("T-Impulse") : F("T-Motion"));
+  Serial.println(F(" (S76G) Power-on Self Test"));
+  Serial.println();
+  Serial.flush();
 
   /* SSD1306 I2C OLED probing */
   Wire.beginTransmission(SSD1306_OLED_I2C_ADDR);
@@ -192,10 +239,38 @@ void setup() {
   if (u8x8) {
     u8x8->begin();
     u8x8->setFont(u8x8_font_chroma48medium8_r);
-    u8x8->clear();
-    u8x8->draw2x2String(2, 1, "SoftRF");
-    u8x8->drawString   (6, 4, "and");
-    u8x8->draw2x2String(2, 6, "LilyGO");
+
+    if (has_IMU) {
+
+      u8x8->draw2x2Glyph(4,  4, SoftRF_text3[0]);
+      u8x8->draw2x2Glyph(6,  4, SoftRF_text3[1]);
+      u8x8->draw2x2Glyph(8,  4, SoftRF_text3[2]);
+      u8x8->draw2x2Glyph(10, 4, SoftRF_text3[3]);
+      u8x8->draw2x2Glyph(6,  6, SoftRF_text3[4]);
+      u8x8->draw2x2Glyph(8,  6, SoftRF_text3[5]);
+
+      delay(2000);
+
+      u8x8->clear();
+      u8x8->draw2x2String( 5, 5, SoftRF_text2);
+
+      delay(2000);
+
+      u8x8->clear();
+      u8x8->draw2x2Glyph(4,  4, SoftRF_text1[0]);
+      u8x8->draw2x2Glyph(6,  4, SoftRF_text1[1]);
+      u8x8->draw2x2Glyph(8,  4, SoftRF_text1[2]);
+      u8x8->draw2x2Glyph(10, 4, SoftRF_text1[3]);
+      u8x8->draw2x2Glyph(6,  6, SoftRF_text1[4]);
+      u8x8->draw2x2Glyph(8,  6, SoftRF_text1[5]);
+
+    } else {
+
+      u8x8->draw2x2String(2, 1, SoftRF_text1);
+      u8x8->drawString   (6, 4, SoftRF_text2);
+      u8x8->draw2x2String(2, 6, SoftRF_text3);
+    }
+
     delay(3000);
   }
 
@@ -226,7 +301,7 @@ void setup() {
   digitalWrite(SOC_GPIO_PIN_SS, HIGH);
 
   SPI.end();
-  pinMode(SOC_GPIO_PIN_SS, INPUT);
+  pinMode(SOC_GPIO_PIN_SS,  INPUT);
   pinMode(SOC_GPIO_PIN_RST, INPUT);
    
   Serial.println(F("Built-in components:"));
@@ -235,8 +310,14 @@ void setup() {
 
   if (u8x8) {
     u8x8->clear();
-    u8x8->draw2x2String(0, 0, "RADIO");
-    u8x8->draw2x2String(14, 0, has_SX1276 ? "+" : "-");
+
+    if (has_IMU) {
+      u8x8->draw2x2Glyph(  4, 4, 'R');
+      u8x8->draw2x2Glyph(  6, 4, has_SX1276 ? '+' : '-');
+    } else {
+      u8x8->draw2x2String( 0, 0, "RADIO");
+      u8x8->draw2x2String(14, 0, has_SX1276 ? "+" : "-");
+    }
   }
 
   Serial3.begin(GNSS_BAUD_RATE);
@@ -265,25 +346,47 @@ void setup() {
   Serial.print(F("GNSS   - ")); Serial.println(has_GNSS ? F("PASS") : F("FAIL"));
 
   if (u8x8) {
-    u8x8->draw2x2String(0, 2, "GNSS");
-    u8x8->draw2x2String(14, 2, has_GNSS ? "+" : "-");
+    if (has_IMU) {
+      u8x8->draw2x2Glyph(  8, 4, 'G');
+      u8x8->draw2x2Glyph( 10, 4, has_GNSS ? '+' : '-');
+    } else {
+      u8x8->draw2x2String( 0, 2, "GNSS");
+      u8x8->draw2x2String(14, 2, has_GNSS ? "+" : "-");
+    }
   }
 
-  Serial.println();
-  Serial.println(F("External components:"));
+  if (!has_IMU) {
+    Serial.println();
+    Serial.println(F("External components:"));
+  }
+
   Serial.print(F("OLED   - ")); Serial.println(has_OLED ? F("PASS") : F("FAIL"));
 
   if (u8x8) {
-    u8x8->draw2x2String(0, 4, "OLED");
-    u8x8->draw2x2String(14, 4, has_OLED ? "+" : "-");
+    if (has_IMU) {
+      u8x8->draw2x2Glyph(4, 6, 'O');
+      u8x8->draw2x2Glyph(6, 6, has_OLED ? '+' : '-');
+    } else {
+      u8x8->draw2x2String( 0, 4, "OLED");
+      u8x8->draw2x2String(14, 4, has_OLED ? "+" : "-");
+    }
   }
 
-  has_BMP280 = bmp_probe();
-  Serial.print(F("BMx280 - ")); Serial.println(has_BMP280 ? F("PASS") : F("FAIL"));
+  if (has_IMU) {
+    Serial.print(F("IMU    - ")); Serial.println(has_IMU    ? F("PASS") : F("FAIL"));
+  } else {
+    has_BMP280 = bmp_probe();
+    Serial.print(F("BMx280 - ")); Serial.println(has_BMP280 ? F("PASS") : F("FAIL"));
+  }
 
   if (u8x8) {
-    u8x8->draw2x2String(0, 6, "BMx280");
-    u8x8->draw2x2String(14, 6, has_BMP280 ? "+" : "-");
+    if (has_IMU) {
+      u8x8->draw2x2Glyph(  8, 6, 'I');
+      u8x8->draw2x2Glyph( 10, 6, has_IMU    ? '+' : '-');
+    } else {
+      u8x8->draw2x2String( 0, 6, "BMx280");
+      u8x8->draw2x2String(14, 6, has_BMP280 ? "+" : "-");
+    }
   }
 
   Wire.end();
